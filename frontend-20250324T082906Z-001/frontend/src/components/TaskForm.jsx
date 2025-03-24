7070eaa983801b0aa9aa6adb010ad2db9b1f634b
kausikimaddula <saikausikimaddula80@gmail.com>
2025-03-24 15:01:31 +0530

    committed

---
+++ frontend-20250324T082906Z-001/frontend/src/components/TaskForm.jsx
@@ -0,0 +1,17 @@
+// src/components/TaskForm.jsx
+import React from 'react';
+import App from "../src/App.jsx"
+
+function TaskForm() {
+    return (
+        <>
+           <div className='taskName'>
+            <h4>title</h4>
+            <h4>description</h4>
+            <h4>completed</h4>
+           </div>
+        </>
+    );
+}
+
+export default TaskForm;
